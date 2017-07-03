7fbdb3a1f73430a59569159dbf5336d79177f998
Abimbola Olaitan <craftwordltd@gmail.com>
2017-07-03 14:38:52 +0100

    modify getPrime.js

--- getPrimes/getPrime.js
+++ getPrimes/getPrime.js
@@ -4,7 +4,7 @@ exports.getPrimes = function (n) {
      if(n == 0 || n < 0) {
      	 return "negative number or zero";
      }
-     else if (n == 1) {
+     else if (n === 1) {
         return "One is not a prime number";
       }
      else {
@@ -12,7 +12,7 @@ exports.getPrimes = function (n) {
      * An integer is prime if it is not divisible by any prime less than or equal to its square root
      **/
       let j,k=2;
-      var arrPrime=[2];
+      let arrPrime=[2];
       for (i=3;i<=n;i+=2){
         j = Math.sqrt(i);
         isPrime = true;
