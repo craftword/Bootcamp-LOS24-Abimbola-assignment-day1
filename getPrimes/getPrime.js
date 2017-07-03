61df61eed368d8fbcc7f97dbc79b555049e1fd2d
Abimbola Olaitan <craftwordltd@gmail.com>
2017-07-03 13:40:49 +0100

    modify getPrime file and getPrime-spec

--- getPrimes/getPrime.js
+++ getPrimes/getPrime.js
@@ -2,13 +2,17 @@ exports.getPrimes = function (n) {
   var arr = [];
   if(Number.isInteger(n)) {
      if(n == 0 || n < 0) {
-     	return "negative number or zero";
-     }else {
+     	 return "negative number or zero";
+     }
+     else if (n == 1) {
+        return "One is not a prime number";
+      }
+     else {
       /**
      * An integer is prime if it is not divisible by any prime less than or equal to its square root
      **/
-      var j,k=2,
-      arrPrime=[2];
+      let j,k=2;
+      var arrPrime=[2];
       for (i=3;i<=n;i+=2){
         j = Math.sqrt(i);
         isPrime = true;
--- spec/getPrime-spec.js
+++ spec/getPrime-spec.js
@@ -17,6 +17,9 @@ describe("Test for Invalid Input", function() {
     it("should return 'Invalid Input' for float 10.7", function() {
       app.getPrimes(10.7).should.be.equal('Invalid Input');
     });
+   it("should return 'Invalid Input' for [2,3,4]", function() {
+      app.getPrimes([2,3,4]).should.be.equal('Invalid Input');
+    }); 
 
 });
 
@@ -24,13 +27,15 @@ describe("Test for Invalid Input", function() {
 describe("Test for Integer", function() {
 
     it("should return 10 for [2,3,5,7]", function() {
-      app.getPrimes(10).should.be.equal([2,3,5,7]);
+      app.getPrimes(10).should.eql([2, 3, 5, 7 ]);
     });
 
     it("should return 50 for [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ]", function() {
-      app.getPrimes(50).should.be.equal([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ]);
+      app.getPrimes(50).should.eql([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47]);
     });
-
+    it("should return 20 for [ 2, 3, 5, 7, 11, 13, 17, 19]", function() {
+      app.getPrimes(20).should.eql([ 2, 3, 5, 7, 11, 13, 17, 19]);
+    });
 
 
 });
@@ -44,7 +49,19 @@ describe("Test for negative number or zero", function() {
     it("should return 'negative number or zero' for -5", function() {
       app.getPrimes(-5).should.be.equal('negative number or zero');
     });
+     it("should return 'negative number or zero' for -15", function() {
+      app.getPrimes(-15).should.be.equal('negative number or zero');
+    });
 
 });
+
+describe("Test for One", function() {
+  it("should return 'One is not a prime number' for 1", function() {
+      app.getPrimes(1).should.be.equal('One is not a prime number');
+    });
+
+});
+
+
 
 
