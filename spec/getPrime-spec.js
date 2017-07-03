08f2486ee9952eca03b43840f3a6031354fdeec7
Abimbola Olaitan <craftwordltd@gmail.com>
2017-07-03 12:17:15 +0100

    modify getPrime-spec.js file

--- spec/getPrime-spec.js
+++ spec/getPrime-spec.js
@@ -1,62 +1,50 @@
-var jasmine = require("jasmine")
+// testing code goes here
+
+'use strict'
+
+var chai = require('chai');
+var should = chai.should();
 var app = require("../getPrimes/getPrime.js");
 
 
-describe("Prime Number tests ", function() {
 
-
-  describe("Test for Invalid Input", function() {
+describe("Test for Invalid Input", function() {
 
     it("should return 'Invalid Input' for 'string'", function() {
-      expect(app.getPrimes('string')).toEqual('Invalid Input');
+      app.getPrimes('string').should.be.equal('Invalid Input');
     });
 
     it("should return 'Invalid Input' for float 10.7", function() {
-      expect(app.getPrimes(10.7)).toEqual('Invalid Input');
+      app.getPrimes(10.7).should.be.equal('Invalid Input');
     });
 
-  });
+});
 
 
-  describe("Test for Integer", function() {
+describe("Test for Integer", function() {
 
     it("should return 10 for [2,3,5,7]", function() {
-      expect(app.getPrimes(10)).toEqual([2,3,5,7]);
+      app.getPrimes(10).should.be.equal([2,3,5,7]);
     });
 
     it("should return 50 for [ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ]", function() {
-      expect(app.getPrimes(50)).toEqual([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ]);
+      app.getPrimes(50).should.be.equal([ 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 ]);
     });
-
-
-
-  });
-
-  describe("Test for negative number or zero", function() {
-
-    it("should return 'negative number or zero' for 0", function() {
-      expect(app.getPrimes(0)).toEqual('negative number or zero');
-    });
-
-    it("should return 'negative number or zero' for -5", function() {
-      expect(app.getPrimes(-5)).toEqual('negative number or zero');
-    });
-
-  });
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 });
+
+describe("Test for negative number or zero", function() {
+
+    it("should return 'negative number or zero' for 0", function() {
+      app.getPrimes(0).should.be.equal('negative number or zero');
+    });
+
+    it("should return 'negative number or zero' for -5", function() {
+      app.getPrimes(-5).should.be.equal('negative number or zero');
+    });
+
+});
+
 
